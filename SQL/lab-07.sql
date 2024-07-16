5f8dc668773176fbef7d93a3b55eb961e00e22ca
Yash-Kakadiya <136950942+Yash-Kakadiya@users.noreply.github.com>
2024-07-16 09:07:05 +0530

    Update lab-07.sql

--- SQL/lab-07.sql
+++ SQL/lab-07.sql
@@ -62,20 +62,104 @@ WHERE JoiningDate>'1991-02-08'
 
 --07. Display average salary of Admin department. 
 
+SELECT AVG(Salary) AS AVG_ADMIN_SALARY
+FROM EMP
+WHERE Department='ADMIN';
+
 --08. Display total salary of HR department. 
+
+SELECT SUM(Salary) AS TOTAL_HR_SALARY
+FROM EMP
+WHERE Department='HR';
 
 --09. Count total number of cities of employee without duplication. 
 
+SELECT COUNT(DISTINCT CITY) AS TOTAL_CITIES
+FROM EMP
+
 --10.  Count unique departments. 
+
+SELECT COUNT(DISTINCT Department) AS UNIQUE_DEPARTMENTS
+FROM EMP
 
 --11.  Give minimum salary of employee who belongs to Ahmedabad. 
 
+SELECT MIN(Salary) AS AHMEDABAD_MIN_SALARY
+FROM EMP
+WHERE CITY='AHMEDABAD';
+
 --12.  Find city wise highest salary. 
+
+SELECT CITY, MAX(SALARY) AS CITY_HIGHEST
+FROM EMP
+GROUP BY CITY
 
 --13.  Find department wise lowest salary. 
 
+SELECT Department, MIN(SALARY) AS Department_LOWEST
+FROM EMP
+GROUP BY Department
+
 --14.  Display city with the total number of employees belonging to each city. 
+
+SELECT CITY, SUM(SALARY) AS TOTAL_EMPS_CITY_WISE
+FROM EMP
+GROUP BY CITY
 
 --15.  Give total salary of each department of EMP table. 
 
+SELECT Department, SUM(SALARY) AS TOTAL_DEPARTMENT_WISE
+FROM EMP
+GROUP BY Department
+
 --16.  Give  average  salary  of  each  department  of  EMP  table  without  displaying  the  respective  department name. 
+
+SELECT AVG(SALARY) AS AVG_DEPARTMENT_WISE
+FROM EMP
+GROUP BY Department
+
+--Part-B
+
+--01. Count the number of employees living in Rajkot.
+
+SELECT COUNT(EID) AS EMPS_IN_RAJKOT
+FROM EMP
+WHERE City='RAJKOT'
+
+--02. Display the difference between the highest and lowest salaries. Label the column DIFFERENCE. 
+
+SELECT (MAX(Salary)-MIN(Salary)) AS DIFFERENCE
+FROM EMP
+
+--03. Display the total number of employees hired before 1st January, 1991.
+
+SELECT COUNT(EID) AS HIRED_BEFORE_1st_January_1991
+FROM EMP
+WHERE JoiningDate<'1991-01-01'
+
+--Part-C
+
+--01. Count the number of employees living in Rajkot or Baroda. 
+
+SELECT COUNT(EID) AS EMPS_IN_RAJKOT_OR_BARODA
+FROM EMP
+WHERE City IN ('RAJKOT','BARODA')
+
+--02. Display the total number of employees hired before 1st January, 1991 in IT department. 
+
+SELECT COUNT(EID) AS EMPS_IN_RAJKOT_OR_BARODA
+FROM EMP
+WHERE JoiningDate<'1991-01-01' AND Department='IT'
+
+--03. Find the Joining Date wise Total Salaries. 
+
+SELECT JoiningDate, SUM(SALARY) AS Joining_Date_Wise_Total_Salaries
+FROM EMP
+GROUP BY JoiningDate
+
+--04. Find the Maximum salary department & city wise in which city name starts with ‘R’.
+
+SELECT Department, CITY, MAX(SALARY) AS MAX_Salary_Department_With_City_Starts_With_R
+FROM EMP
+WHERE CITY LIKE 'R%'
+GROUP BY  DEPARTMENT,CITY
