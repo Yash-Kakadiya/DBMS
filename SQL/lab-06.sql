ef5fa95bdde924ada65be4ff5ff27449ad2c380f
Yash-Kakadiya <136950942+Yash-Kakadiya@users.noreply.github.com>
2024-08-06 09:10:36 +0530

    Update lab-06.sql

--- SQL/lab-06.sql
+++ SQL/lab-06.sql
@@ -204,3 +204,225 @@ SELECT REVERSE('DARSHAN')
 
 SELECT REPLICATE('YASH ',3)
 
+
+-- Part – B: Perform following queries on Student table of practical no 5. 
+
+SELECT *
+FROM STUDENT
+
+-- 01. Find the length of FirstName and LastName columns. 
+
+SELECT LEN(FIRSTNAME) AS FIRSTNAME, LEN(LASTNAME) AS LASTNAME
+FROM STUDENT
+
+-- 02. Display FirstName and LastName columns in lower & upper case. 
+
+SELECT UPPER(FIRSTNAME) AS UPPER_FIRSTNAME, UPPER(LASTNAME) AS UPPER_LASTNAME
+FROM STUDENT
+
+SELECT LOWER(FIRSTNAME) AS LOWER_FIRSTNAME, LOWER(LASTNAME) AS LOWER_LASTNAME
+FROM STUDENT
+
+-- 03. Display first three characters of FirstName column. 
+
+SELECT LEFT(FIRSTNAME,3) AS FIRST_THREE
+FROM STUDENT
+
+
+-- 04. Display 3rd to 10th character of Website column. 
+
+SELECT SUBSTRING(WEBSITE,3,10-3)
+FROM STUDENT
+
+-- 05. Write a query to display first 4 & Last 5 characters of Website column.
+
+SELECT LEFT(WEBSITE,4) AS FIRST, RIGHT(WEBSITE,5)AS LAST
+FROM STUDENT
+
+
+-- Part – C: Perform following queries on Student table of practical no 5.
+
+SELECT *
+FROM STUDENT
+
+-- 01. Put 10 space before FirstName using function. 
+
+SELECT SPACE(10)+FIRSTNAME
+FROM STUDENT
+
+-- 02. Combine FirstName and LastName columns using + sign as well as CONCAT (). 
+
+SELECT (FIRSTNAME+' '+LASTNAME) AS FULLNAME
+FROM STUDENT
+
+SELECT CONCAT(FIRSTNAME,' ',LASTNAME) AS FULLNAME
+FROM STUDENT
+
+-- 03. Combine all columns using + sign as well as CONCAT (). 
+
+SELECT ((CAST (STUID AS varchar(10)))+' '+FIRSTNAME+' '+LASTNAME+' '+WEBSITE+' '+CITY+' '+ADDRESS) AS COMBINEALL
+FROM STUDENT
+
+SELECT CONCAT(STUID,' ',FIRSTNAME,' ',LASTNAME,' ',WEBSITE,' ',CITY,' ',ADDRESS) AS COMBINEALL
+FROM STUDENT
+
+-- 04. Find reverse of FirstName column. 
+
+SELECT REVERSE(FIRSTNAME)
+FROM STUDENT
+
+-- 05. Repeat FirstName column 3 times 
+
+SELECT REPLICATE(FIRSTNAME,3)
+FROM STUDENT
+
+-- 06. Give the Names which contains 5 characters. 
+
+SELECT FIRSTNAME
+FROM STUDENT
+WHERE LEN(FIRSTNAME)=5
+
+-- 07. Combine the result as <FirstName> Lives in <City>. 
+
+SELECT CONCAT(FIRSTNAME,' lives in ',CITY)
+FROM STUDENT
+
+-- 08. Combine the result as Student_ID of < FirstName > is <StuID> .
+
+SELECT CONCAT('Student_Id of',FIRSTNAME,' is ',STUID)
+FROM STUDENT
+
+
+---Date Functions
+
+-- Part – A: 
+
+-- 01. Write a query to display the current date & time. Label the column Today_Date. 
+
+SELECT GETDATE() AS TODAY_DATE
+
+-- 02. Write a query to find new date after 365 day with reference to today. 
+
+SELECT DATEADD(DAY,365,GETDATE()) AS AFTERYEAR
+
+-- 03. Display the current date in a format that appears as may 5 1994 12:00AM. 
+
+SELECT FORMAT(GETDATE(),'MMMM d yyyy')
+
+-- 04. Display the current date in a format that appears as 03 Jan 1995. 
+
+SELECT FORMAT(GETDATE(),'dd MMM yyyy')
+
+-- 05. Display the current date in a format that appears as Jan 04, 96. 
+
+SELECT FORMAT(GETDATE(),'MMM dd, yy')
+
+-- 06. Write a query to find out total number of months between 31-Dec-08 and 31-Mar-09. 
+
+SELECT DATEDIFF(MONTH,'31-DEC-08','31-MAR-09')
+
+-- 07. Write a query to find out total number of years between 25-Jan-12 and 14-Sep-10. 
+
+SELECT DATEDIFF(YEAR,'14-Sep-10','25-Jan-12')
+
+-- 08. Write a query to find out total number of hours between 25-Jan-12 7:00 and 26-Jan-12 10:30. 
+
+SELECT DATEDIFF(HOUR,'25-Jan-12 7:00','26-Jan-12 10:30')
+
+-- 09. Write a query to extract Day, Month, Year from given date 12-May-16. 
+
+SELECT DATEPART(DAY,'12-May-16') AS DAY, DATEPART(MONTH,'12-May-16') AS MONTH, DATEPART(YEAR,'12-May-16') AS YEAR
+
+-- 10.  Write a query that adds 5 years to current date. 
+
+SELECT DATEADD(YEAR,5,GETDATE())
+
+-- 11.  Write a query to subtract 2 months from current date. 
+
+SELECT DATEADD(MONTH,-2,GETDATE())
+
+-- 12.  Extract month from current date using datename () and datepart () function. 
+
+SELECT DATENAME(MONTH,GETDATE()) AS MONTH
+SELECT DATEPART(MONTH,GETDATE()) AS MONTH
+
+-- 13.  Write a query to find out last date of current month. 
+
+SELECT EOMONTH(GETDATE()) AS LAST_DATE
+
+-- 14.  Calculate your age in years and months.
+
+SELECT DATEDIFF(YEAR,'2005-12-01',GETDATE())
+SELECT DATEDIFF(MONTH,'2005-12-01',GETDATE())
+
+
+--PART-B
+-- Create a table EMP_DETAIL and insert the following records in the table.
+CREATE TABLE EMP_DETAIL
+(
+    EmpNo int,
+    EmpName varchar(50),
+    JoiningDate datetime,
+    Salary decimal(7,2),
+    City varchar(50)
+);
+INSERT INTO EMP_DETAIL
+    (EmpNo,EmpName,JoiningDate,Salary,City)
+VALUES
+    (101, 'Keyur', '2002-01-15', 12000.00, 'Rajkot'),
+    (102, 'Hardik', '2004-02-15', 14000.00, 'Ahmedabad'),
+    (103, 'Kajal', '2006-03-14', 15000.00, 'Baroda'),
+    (104, 'Bhoomi', '2005-06-23', 12500.00, 'Ahmedabad'),
+    (105, 'Harmit', '2004-02-15', 14000.00, 'Rajkot'),
+    (106, 'Jay', '2007-03-12', 12000.00, 'Surat');
+
+SELECT *
+FROM EMP_DETAIL
+
+--01 Write a query to find new date after 365 day with reference to JoiningDate.
+SELECT DATEADD(DAY,365,JoiningDate)
+FROM EMP_DETAIL;
+
+--02 Display the JoiningDate in a format that appears as may 5 1994 12:00AM
+SELECT FORMAT(JOININGDATE,'MMMM d yyyy hh:mmtt')
+FROM EMP_DETAIL;
+
+--03 Display the JoiningDate in a format that appears as 03 Jan 1995.
+SELECT FORMAT(JOININGDATE,'dd MMM yyyy')
+FROM EMP_DETAIL;
+
+--04 Display the JoiningDate in a format that appears as Jan 04, 96
+SELECT FORMAT(JOININGDATE,'MMM dd, yy')
+FROM EMP_DETAIL;
+
+--05 Write a query to find out total number of months between JoiningDate and 31-Mar-09.
+SELECT DATEDIFF(MONTH,JOININGDATE,'2009-03-31')
+FROM EMP_DETAIL;
+
+--06 Write a query to find out total number of years between JoiningDate and 14-Sep-10
+SELECT DATEDIFF(YEAR,JOININGDATE,'2010-09-14')
+FROM EMP_DETAIL;
+
+--PART-C
+
+--01 Write a query to extract Day, Month, Year from JoiningDate
+SELECT DAY(JOININGDATE) AS DAY, MONTH(JOININGDATE) AS MONTH, YEAR(JOININGDATE) AS YEAR
+FROM EMP_DETAIL;
+
+--2 Write a query that adds 5 years to JoiningDate.
+SELECT DATEADD(YEAR,5,JOININGDATE)
+FROM EMP_DETAIL;
+
+--3 Write a query to subtract 2 months from JoiningDate
+SELECT DATEADD(MONTH,-2,JOININGDATE)
+FROM EMP_DETAIL;
+
+--4 Extract month from JoiningDate using datename () and datepart () function
+SELECT DATENAME(MONTH,JOININGDATE), DATEPART(MONTH,JOININGDATE)
+FROM EMP_DETAIL;
+
+--5 Calculate your age in years and months
+
+SELECT DATEDIFF(YEAR,'2005-12-01',GETDATE());
+SELECT DATEDIFF(MONTH,'2005-12-01',GETDATE());
+
